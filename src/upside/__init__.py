c9a4c8f7e837ef6a8cf835f17ddf33088a3e2001
josh <josh.lin@davionlabs.com>
2026-09-18 10:56:39 +0800

    Release 0.1.1
    Covers the endpoints added in the 2026-09-10 API reference: six write actions,
    five info queries, three WebSocket channels, trigger orders and inline TP/SL.

--- src/upside/__init__.py
+++ src/upside/__init__.py
@@ -23,7 +23,7 @@ from .utils.signing import NonceManager
 from .utils.types import Cloid
 from .websocket_manager import WebsocketManager
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 __all__ = [
     "API",
